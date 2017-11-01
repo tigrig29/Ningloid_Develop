d391702293de78137ff21f1a67ea3c2baf936179
TigRig <tigrig.crex@gmail.com>
2017-11-01 17:53:28 +0900

    フォルダ分け
    ゲーム用のデータ（game/data, game/libs, game/ningloid）を、新たに作成したresourcesフォルダへ移行

--- editor/js/ningloidEditor.js
+++ editor/js/ningloidEditor.js
@@ -1,8 +1,6 @@
-// テスト：エディタとゲームを同時起動する
-const electron = require("electron");
-const {BrowserWindow} = require("electron").remote;
-const windowOptions = require("./window_options.js");
-windowOptions.width *= 0.7;
-windowOptions.height *= 0.7;
-let win = new BrowserWindow(windowOptions);
-win.loadURL(`file://${__dirname}/../game/index.html`);
+// // テスト：２窓式
+// const {BrowserWindow} = require("electron").remote;
+// const windowOptions = require("../game/window_options.js");
+// let win = new BrowserWindow(windowOptions);
+// win.loadURL(`file://${__dirname}/../game/index.html`);
+
